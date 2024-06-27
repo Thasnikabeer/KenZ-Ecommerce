8e1ec90194e1c0bcb36a1efc42ea92acbcc87980
Thasneema <thaznitaimoor@gmail.com>
2024-06-27 11:42:53 +0400

    third commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,10 @@ const bcrypt = require('bcrypt');
 const nodemailer= require("nodemailer");
 const config  = require("../config/config")
 const randomstring = require("randomstring");
+
+
 console.log("second commit changes ")
+console.log("third commit changes ")
 //generate hashed password
 const securePassword = async(password)=>{
     try{
